335a17d146c4b17e33be666d0b70800af6c9f2a1
315373247 <315373247@qq.com>
2026-03-20 08:50:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ client = OpenAI(
 completion = client.chat.completions.create(
     model="gpt-3.5-turbo-16k",
     messages=[
-        {"role": "system", "content": "你是OpenAi开发的聊天机器人，请根据用户的输入回复对应的信息，谢谢"},
+        {"role": "system", "content": "你是OpenAi开发的聊天机器人，请根据用户的输入回复对应的信息"},
         {"role": "user", "content": query},
     ],
 )
